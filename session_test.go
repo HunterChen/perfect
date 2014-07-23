1d109a2c6dc5644418d32921ddca1a63609802b9
Victor Petrov <victor_petrov@harvard.edu>
2014-07-23 14:14:48 -0400

    Fix old instance of 'db' in session_test.go

--- session_test.go
+++ session_test.go
@@ -1,32 +1,32 @@
 package perfect
 
 import (
+	"github.com/vpetrov/perfect/orm"
 	"testing"
-    "github.com/vpetrov/perfect/orm"
 )
 
 var mock_session *Session = &Session{
-    Object: orm.Object{Id:1},
-	Id:            db.String("ABCD"),
-	Authenticated: db.Bool(true),
+	Object:        orm.Object{Id: 1},
+	Id:            orm.String("ABCD"),
+	Authenticated: orm.Bool(true),
 	Values:        &map[string]string{"id": "ABCD", "authenticated": "1"},
 }
 
 func TestNewSession(t *testing.T) {
-    session_id := "test"
+	session_id := "test"
 	session := NewSession(session_id)
 
 	if session.Id == nil {
-        t.Fatalf("session.Id is nil, expected non-nil")
+		t.Fatalf("session.Id is nil, expected non-nil")
 	}
 
-    if *session.Id != session_id {
-        t.Fatalf("*session.Id is '%v', expected '%v'", *session.Id, session_id)
-    }
+	if *session.Id != session_id {
+		t.Fatalf("*session.Id is '%v', expected '%v'", *session.Id, session_id)
+	}
 
-    if session.Authenticated == nil {
-        t.Fatalf("session.Authenticated is nil, expected non-nil")
-    }
+	if session.Authenticated == nil {
+		t.Fatalf("session.Authenticated is nil, expected non-nil")
+	}
 
 	if *session.Authenticated {
 		t.Fatalf("*session.Authenticated is %v, want %v", *session.Authenticated, false)
@@ -36,4 +36,3 @@ func TestNewSession(t *testing.T) {
 		t.Fatalf("session.Values is not allocated")
 	}
 }
-
